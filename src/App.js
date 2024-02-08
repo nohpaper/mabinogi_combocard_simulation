b8ca6bcc2e61d396cecd94730094ca47fb778ccd
nohpaper <nohpaper99@naver.com>
2024-02-08 14:50:37 +0900

    feat : footer 작업중

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer.js';
 
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
 
 
+        <section>
+            <h1>커스텀마이징할 콤보카드 입력</h1>
+        </section>
 
       <Routes>
         <Route></Route>
       </Routes>
 
 
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 }
 
 
-function Footer() {
 
-}
 export default App;
