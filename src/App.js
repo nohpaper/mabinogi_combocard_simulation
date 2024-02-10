f6c5062c08fccff79556ff1146f1e492bf50fef0
Noh jonghee <nohpaper99@naver.com>
2024-02-10 18:06:35 +0900

    feat : ui 진행중 sass확인 해야함,

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.css';
+import './stylesheet/App.scss';
+import './stylesheet/defalut.scss';
 import Footer from './Footer.js';
 
 import { Route, Routes } from 'react-router-dom';
@@ -7,21 +8,49 @@ function App() {
   return (
     <div className="App">
 
+        <div className="wrap">
+            <section className="input_step">
+                <h1>커스텀마이징할 콤보카드 입력</h1>
+                <div className="container">
+                    <h5>콤보 카드 입력창</h5>
+                    <div className="input_wrap">
+                        <ul>
+                            <li className="active">일반</li>
+                            <li>골든</li>
+                            <li>블랙</li>
+                        </ul>
+                        <div className="input_inner">
+                            <div className="combocard_wrap">
 
-        <section>
-            <h1>커스텀마이징할 콤보카드 입력</h1>
-        </section>
+                            </div>
+                            <ul className="skill_wrap">
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                                <li><button></button></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
 
-      <Routes>
+
+        {/*<Routes>
         <Route></Route>
-      </Routes>
+      </Routes>*/}
 
 
-      <Footer />
+        <Footer/>
     </div>
   );
 }
-
 
 
 export default App;
