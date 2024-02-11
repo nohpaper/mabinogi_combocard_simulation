d8a8c60bd7249d9ff1ed6e4cbb8625ddc7e9c805
Noh jonghee <nohpaper99@naver.com>
2024-02-11 13:08:59 +0900

    feat : ui 진행중 sass확인 해야함,

--- src/App.js
+++ src/App.js
@@ -21,20 +21,75 @@ function App() {
                         </ul>
                         <div className="input_inner">
                             <div className="combocard_wrap">
-
+                                <div className="combo_each">
+                                    <div className="active_wrap">
+                                        <button type="button"></button>
+                                    </div>
+                                </div>
+                                <div className="combo_each">
+                                    <button type="button" className="disabled_wrap">추가</button>
+                                    <div className="active_wrap">
+                                        <button type="button"></button>
+                                        <div className="combo_input">
+                                            <input type="text" maxLength="2"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="combo_each">
+                                    <button type="button" className="disabled_wrap">추가</button>
+                                    <div className="active_wrap disabled">
+                                        <div className="combo_input">
+                                        <input type="text" maxLength="2"/>
+                                    </div>
+                                    <button type="button"></button>
+                                    </div>
+                                </div>
+                                <div className="combo_each">
+                                    <button type="button" className="disabled_wrap">추가</button>
+                                    <div className="active_wrap disabled">
+                                        <button type="button"></button>
+                                    <div className="combo_input">
+                                        <input type="text" maxLength="2"/>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div className="combo_each">
+                                    <button type="button" className="disabled_wrap">추가</button>
+                                    <div className="active_wrap disabled">
+                                        <div className="combo_input">
+                                        <input type="text" maxLength="2"/>
+                                    </div>
+                                    <button type="button"></button>
+                                    </div>
+                                </div>
+                                <div className="combo_each">
+                                    <button type="button" className="disabled_wrap">추가</button>
+                                    <div className="active_wrap disabled">
+                                        <button type="button"></button>
+                                    <div className="combo_input">
+                                        <input type="text" maxLength="2"/>
+                                    </div>
+                                    </div>
+                                </div>
+                                <div className="combo_arrow"></div>
                             </div>
                             <ul className="skill_wrap">
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
-                                <li><button></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
+                                <li><button type="button"></button></li>
                             </ul>
+                        </div>
+                        <div className="btn_wrap">
+                            <button type="button" className="next_step">제거</button>
+                            <button type="button" className="next_step">완성</button>
+                            <button type="button" className="reset_step">초기화</button>
                         </div>
                     </div>
                 </div>
